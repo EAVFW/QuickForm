7c3417e99462a271cb3f3d8e3ac428fd8dead3e0
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-02-07 10:13:10 +0100

    fix: fixed issues with state on slides

--- packages/designer/src/Components/Views/QuickFormLayoutView.tsx
+++ packages/designer/src/Components/Views/QuickFormLayoutView.tsx
@@ -1,11 +1,13 @@
 import { mergeClasses } from "@fluentui/react-components";
 import { useViewStyles } from "../Styles/useViewStyles.styles"
 import { PageDesignEditor, CraftEditor, CraftViewPort, useEditorChanges } from "@eavfw/designer";
-import { QuickFormDef } from "../../QuickFormDesigner";
-
+ 
 import { useEffect, useMemo } from "react";
 import { removeNonAlphanumeric } from "@eavfw/utils";
 import { SerializedNodes } from "@craftjs/core"
+import { QuickFormDef } from "../../Types/QuickFormDefinition";
+
+
 
 const initial = JSON.stringify(
     {
@@ -60,7 +62,88 @@ export const QuickFormLayoutView = ({ dispatch, slideId, layout }: {
 
 
     useEffect(() => {
-        console.log("CraftEditor6", [slideId, store.history.timeline, nodes]);
+         
+        
+
+        //console.log("CraftEditor11", [slideId, layout, JSON.parse(JSON.stringify(store.history.timeline))]);
+        //let last = store.history.timeline[store.history.timeline.length - 1];
+        //let lastPatch = last && last.patches[last.patches.length - 1];
+        //if (lastPatch && lastPatch.op === "replace" && lastPatch.path[lastPatch.path.length - 1] === "displayName") {
+        //    return;
+        //}
+
+
+         {
+            const rownodes = slideId && layout.slides[slideId]?.rows ?
+                Object.fromEntries(
+                    Object.entries(
+                        layout.slides[slideId]?.rows ?? {
+                        }).map(([rowid, row]) => [rowid, {
+                            "type": { "resolvedName": "Question" },
+                            "isCanvas": true, "props": { questionid: row.columns["column1"]["q"].ref },
+                            "displayName": "Question",
+                            "custom": { "displayName": "Question" },
+                            "parent": "ROOT",
+                            "hidden": false,
+                            "nodes": [],
+                            "linkedNodes": {}
+                        }])) : {
+                    "GBCxB0dj1x": {
+                        "type": { "resolvedName": "GridNode" },
+                        "isCanvas": true, "props": {},
+                        "displayName": "Grid",
+                        "custom": { "displayName": "Content" },
+                        "parent": "ROOT",
+                        "hidden": false,
+                        "nodes": [],
+                        "linkedNodes": {}
+                    }
+                };
+
+            const data = JSON.stringify({
+                "ROOT": {
+                    "type": { "resolvedName": "Container" },
+                    "isCanvas": true,
+                    "props": {
+                        "flexDirection": "column",
+                        "alignItems": "flex-start",
+                        "justifyContent": "flex-start",
+                        "fillSpace": "no",
+                        "padding": ["40", "40", "40", "40"],
+                        "margin": ["0", "0", "0", "0"],
+                        "background": { "r": 255, "g": 255, "b": 255, "a": 1 },
+                        "color": { "r": 0, "g": 0, "b": 0, "a": 1 },
+                        "shadow": 0,
+                        "radius": 0,
+                        "width": "90%",
+                        "height": "auto"
+                    },
+                    "displayName": "Container",
+                    "custom": {
+                        renderNode: {
+                            canAddLeft: false,
+                            canAddRight: false,
+                            canAddTop: true,
+                            canAddBottom: true
+                        },
+                        slideId: slideId,
+                        "displayName": slideId && slideId in layout.slides ? layout.slides[slideId].title : 'New Slide *'
+                    },
+                    "hidden": false,
+                    "nodes": Object.keys(rownodes),
+                    "linkedNodes": {}
+                },
+                ...rownodes
+            });
+            console.log("CraftEditor10", [slideId, layout, JSON.parse(data)]);
+
+            deserialize(data);
+            history.clear();
+        };
+    }, [slideId]);
+
+    useEffect(() => {
+        console.log("CraftEditor6", [slideId, store.history.timeline, nodes, nodes.ROOT?.custom?.displayName, slideId && layout?.slides?.[slideId]?.title]);
 
 
         /**
@@ -80,18 +163,20 @@ export const QuickFormLayoutView = ({ dispatch, slideId, layout }: {
                     let schemaName = removeNonAlphanumeric(title);
                     let logicalName = schemaName.toLowerCase();
 
-                    old.layout.slides[logicalName] = { ...old.layout.slides[oldslideid], title, schemaName, logicalName };
-                    if (logicalName !== oldslideid)
-                        delete old.layout.slides[oldslideid];
-
+                    old.layout.slides[oldslideid] = { ...old.layout.slides[oldslideid], title, schemaName, logicalName };
+                     
                     if (!old.__designer)
                         old.__designer = {};
 
-                    old.__designer.activeSlide = logicalName;
+                    old.__designer.activeSlide = oldslideid;
                     return { ...old };
                 });
             }
         }
+
+        /**
+         * populate the layout based on designer state.
+         */
 
         if (nodes?.ROOT && nodes.ROOT.custom.slideId === slideId) {
             dispatch(quickform => {
@@ -131,83 +216,7 @@ export const QuickFormLayoutView = ({ dispatch, slideId, layout }: {
 
     }, [nodes, slideId, layout]);
 
-      useEffect(() => {
-
-
-          console.log("CraftEditor11", [slideId, layout, JSON.parse(JSON.stringify(store.history.timeline))]);
-          let last = store.history.timeline[store.history.timeline.length - 1];
-          let lastPatch = last && last.patches[last.patches.length - 1];
-          if (lastPatch && lastPatch.op === "replace" && lastPatch.path[lastPatch.path.length - 1] === "displayName") {
-              return;
-          }
-
-          const nodes = slideId && layout.slides[slideId]?.rows ?
-            Object.fromEntries(
-                Object.entries(
-                    layout.slides[slideId]?.rows ?? {
-                    }).map(([rowid, row]) => [rowid, {
-                        "type": { "resolvedName": "Question" },
-                        "isCanvas": true, "props": { questionid: row.columns["column1"]["q"].ref },
-                        "displayName": "Question",
-                        "custom": { "displayName": "Question" },
-                        "parent": "ROOT",
-                        "hidden": false,
-                        "nodes": [],
-                        "linkedNodes": {}
-                    }])) : {
-                "GBCxB0dj1x": {
-                    "type": { "resolvedName": "GridNode" },
-                    "isCanvas": true, "props": {},
-                    "displayName": "Grid",
-                    "custom": { "displayName": "Content" },
-                    "parent": "ROOT",
-                    "hidden": false,
-                    "nodes": [],
-                    "linkedNodes": {}
-                }
-            };
-
-          const data = JSON.stringify( {
-            "ROOT": {
-                "type": { "resolvedName": "Container" },
-                "isCanvas": true,
-                "props": {
-                    "flexDirection": "column",
-                    "alignItems": "flex-start",
-                    "justifyContent": "flex-start",
-                    "fillSpace": "no",
-                    "padding": ["40", "40", "40", "40"],
-                    "margin": ["0", "0", "0", "0"],
-                    "background": { "r": 255, "g": 255, "b": 255, "a": 1 },
-                    "color": { "r": 0, "g": 0, "b": 0, "a": 1 },
-                    "shadow": 0,
-                    "radius": 0,
-                    "width": "90%",
-                    "height": "auto"
-                },
-                "displayName": "Container",
-                "custom": {
-                    renderNode: {
-                        canAddLeft: false,
-                        canAddRight: false,
-                        canAddTop: true,
-                        canAddBottom: true
-                    },
-                    slideId: slideId,
-                    "displayName": slideId && slideId in layout.slides ? layout.slides[slideId].title : 'New Slide *'
-                },
-                "hidden": false,
-                "nodes": Object.keys(nodes),
-                "linkedNodes": {}
-            },
-            ...nodes
-          });
-          console.log("CraftEditor10", [slideId, layout, JSON.parse(data)]);
-
-          deserialize(data);
-          history.clear();
-
-    }, [slideId]);
+    
 
     if (slideId) {
         console.log("CraftEditor9", [layout.slides, slideId]);
