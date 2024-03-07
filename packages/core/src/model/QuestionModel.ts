ec63d42089e7cd88c328aa9ddd2e6168165bba8b
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-03-07 22:28:00 +0100

    fix: type error that paragraph might be undefined. Paragraph is not required and type should reflect this.

--- packages/core/src/model/QuestionModel.ts
+++ packages/core/src/model/QuestionModel.ts
@@ -4,13 +4,26 @@ export type QuestionModel<TProps = InputPropertiesTypes> = {
     answered: boolean;
     dataType: "string" | "number" | "boolean";
     inputProperties?: TProps;
-    inputType: keyof InputTypeMap | "text" | string;
+
+    /**
+     * The inputType can be any of the known input types, 
+     * a string for types unkonwn to quickform but registered as custom question components. 
+     * 
+     * TODO - decide what we call the components. "Input Components", "Question Components" ect. What is a good name for documentation.
+     */
+    inputType: keyof InputTypeMap | string;
     /**
      * The logical name is the name used in payloads and serialization
     */
     logicalName: string;
     output: any;
-    paragraph: string;
+
+    /**
+    * The paragraph text that is used to render addition context to the question text <see>text</see> 
+    * This can be optional and not rended when undefined.
+    */
+    paragraph?: string;
+   
     placeholder: string;
     /**
      * The question key is the key used in the json model, its the key that is used from layout to reference a given question. 
