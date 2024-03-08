280b99421fd3eeb191eeb53bb922288932afa467
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-03-08 09:20:34 +0100

    fix: export default quickform tokens for designer

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,4 @@ export * from "./state";
 export * from "./services";
 export * from "./hooks";
 export type { QuickFormDefinition, InputProps } from "./model";
+export {  defaultQuickFormTokens } from "./model";
