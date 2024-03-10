47df17421f4efeb1d01df1c58e191124983ebc6c
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-03-10 17:14:42 +0100

    fix: added color for error box text to use onError

--- packages/core/src/components/error-message/ErrorMessage.tsx
+++ packages/core/src/components/error-message/ErrorMessage.tsx
@@ -2,63 +2,64 @@
 import React, { useState, useEffect } from 'react';
 import { useDelayedClickListener } from "../../hooks";
 import { useQuickForm } from "../../state/QuickFormContext";
+import { quickformtokens } from '../../style/quickformtokens';
 
 type ErrorProps = {
-  readonly message: string;
+    readonly message: string;
 };
 
 export const ErrorMessage: React.FC<ErrorProps> = ({ message }: ErrorProps) => {
-  const [isVisible, setIsVisible] = useState(false);
-  const { dispatch, state } = useQuickForm();
+    const [isVisible, setIsVisible] = useState(false);
+    const { dispatch, state } = useQuickForm();
 
-  const resetErrorMessage = () => {
-    if (state.errorMsg !== "") {
-      dispatch({ type: "SET_ERROR_MSG", msg: "" })
+    const resetErrorMessage = () => {
+        if (state.errorMsg !== "") {
+            dispatch({ type: "SET_ERROR_MSG", msg: "" })
+        }
     }
-  }
 
-  useDelayedClickListener(resetErrorMessage);
+    useDelayedClickListener(resetErrorMessage);
 
-  useEffect(() => {
-    if (message) {
-      setIsVisible(true);
-      setTimeout(() => setIsVisible(false), 350);
+    useEffect(() => {
+        if (message) {
+            setIsVisible(true);
+            setTimeout(() => setIsVisible(false), 350);
+        }
+    }, [message]);
+
+    if (message === "") {
+        return <></>;
     }
-  }, [message]);
 
-  if (message === "") {
-    return <></>;
-  }
+    const errorStyle: React.CSSProperties = {
+        alignItems: 'flex-end',
+        animation: isVisible ? 'slide-up 0.35s linear 1 forwards' : '',
+        backgroundColor: 'var(--surface)',
+        borderRadius: '3px',
+        color: quickformtokens.onError,
+        display: 'flex',
+        fontSize: '1.5rem',
+        marginTop: '15px',
+        padding: '8px 12px',
+        width: 'max-content',
+    };
 
-  const errorStyle: React.CSSProperties = {
-    alignItems: 'flex-end',
-    animation: isVisible ? 'slide-up 0.35s linear 1 forwards' : '',
-    backgroundColor: 'var(--surface)',
-    borderRadius: '3px',
-    color: 'var(--error)',
-    display: 'flex',
-    fontSize: '1.5rem',
-    marginTop: '15px',
-    padding: '8px 12px',
-    width: 'max-content',
-  };
+    const mobileErrorStyle: React.CSSProperties = {
+        ...errorStyle,
+        fontSize: '1.75rem',
+        marginTop: '22px',
+        width: '100%',
+    };
 
-  const mobileErrorStyle: React.CSSProperties = {
-    ...errorStyle,
-    fontSize: '1.75rem',
-    marginTop: '22px',
-    width: '100%',
-  };
+    const imgStyle: React.CSSProperties = {
+        marginRight: '4px',
+    };
 
-  const imgStyle: React.CSSProperties = {
-    marginRight: '4px',
-  };
-
-  return (
-    <div style={window.innerWidth <= 599 ? mobileErrorStyle : errorStyle}>
-      {/* If there's an image you want to include inside the error message */}
-      {/* <img src="path_to_your_image" alt="Error" style={imgStyle} /> */}
-      {message}
-    </div>
-  );
+    return (
+        <div style={window.innerWidth <= 599 ? mobileErrorStyle : errorStyle}>
+            {/* If there's an image you want to include inside the error message */}
+            {/* <img src="path_to_your_image" alt="Error" style={imgStyle} /> */}
+            {message}
+        </div>
+    );
 };
