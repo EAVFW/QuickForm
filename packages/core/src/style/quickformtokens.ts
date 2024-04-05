f2f385ef1efd9839f67fc3358aa01ffba1918425
Kasper Baun <kasperbaun@hotmail.com>
2024-04-05 11:18:17 +0200

    fix: typed quickform tokens so they are able to have more nuances configured for each color

--- packages/core/src/style/quickformtokens.ts
+++ packages/core/src/style/quickformtokens.ts
@@ -1,11 +1,12 @@
-import { camelToKebabCase } from "../utils/quickformUtils";
+import { camelToKebabCase, defineVariables } from "../utils/quickformUtils";
 
 type Color = string;
 type FontSize = string;
 type Gap = string;
+type NuancedColor<T extends string> = `${T}${'Darker' | 'Lighter'}${'' | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900}`;
+type QuickFormStructuredColorProperties = 'primary' | 'onPrimary' | 'secondary' | 'onSecondary' | 'background' | 'onBackground' | 'surface' | 'onSurface' | 'error' | 'onError';
 
-export type QuickFormTokens = {
-
+type QuickFormTokensBase = {
     /* Pure colors */
     white: Color,
     black: Color,
@@ -13,8 +14,6 @@ export type QuickFormTokens = {
 
     /* Structured colors */
     primary: Color,
-    primaryLighter: string,
-    primaryVariant: Color,
     onPrimary: Color,
 
     secondary: Color,
@@ -22,17 +21,12 @@ export type QuickFormTokens = {
 
     background: Color,
     onBackground: Color,
-    // onBackgroundDarker100: string,
-    // onBackgroundDarker900: string,
-    onBackgroundLighter: string,
 
     surface: Color,
     onSurface: Color,
 
     error: string,
     onError: Color,
-    // onErrorDarker100: string,
-    // onErrorDarker900: string,
 
     /* Font-sizes and family */
     fontFamily: string,
@@ -50,43 +44,45 @@ export type QuickFormTokens = {
     borderColor: string
 };
 
+export type QuickFormTokens = QuickFormTokensBase & {
+    [Property in NuancedColor<QuickFormStructuredColorProperties>]?: Color;
+};
+
+
 export const defaultQuickFormTokens: QuickFormTokens = {
 
     white: '#ffffff',
     black: '#000',
     primary: '#154068',
     primaryLighter: 'color-mix(in srgb, var(--primary) 90%, white)',
-    primaryVariant: '#112D4D',
     secondary: '#24517b',
     background: '#ffffff',
     surface: '#c9c9c9',
 
     error: 'var(--surface)',
     onError: '#ff0000',
-    // @pks - This could be great to have a dialog about - are we sticking to the color-structure convention with primary, onPrimary and so on or are we intertwining things?
-
-    // onErrorDarker100: 'color-mix(in srgb, var(--on-error) 90%, black)',
-    // onErrorDarker200: 'color-mix(in srgb, var(--on-error) 80%, black)',
-    // onErrorDarker300: 'color-mix(in srgb, var(--on-error) 70%, black)',
-    // onErrorDarker400: 'color-mix(in srgb, var(--on-error) 60%, black)',
-    // onErrorDarker500: 'color-mix(in srgb, var(--on-error) 50%, black)',
-    // onErrorDarker600: 'color-mix(in srgb, var(--on-error) 40%, black)',
-    // onErrorDarker700: 'color-mix(in srgb, var(--on-error) 30%, black)',
-    // onErrorDarker800: 'color-mix(in srgb, var(--on-error) 20%, black)',
-    // onErrorDarker900: 'color-mix(in srgb, var(--on-error) 10%, black)',
+    onErrorDarker100: 'color-mix(in srgb, var(--on-error) 90%, black)',
+    onErrorDarker200: 'color-mix(in srgb, var(--on-error) 80%, black)',
+    onErrorDarker300: 'color-mix(in srgb, var(--on-error) 70%, black)',
+    onErrorDarker400: 'color-mix(in srgb, var(--on-error) 60%, black)',
+    onErrorDarker500: 'color-mix(in srgb, var(--on-error) 50%, black)',
+    onErrorDarker600: 'color-mix(in srgb, var(--on-error) 40%, black)',
+    onErrorDarker700: 'color-mix(in srgb, var(--on-error) 30%, black)',
+    onErrorDarker800: 'color-mix(in srgb, var(--on-error) 20%, black)',
+    onErrorDarker900: 'color-mix(in srgb, var(--on-error) 10%, black)',
 
     onPrimary: '#ffffff',
     onSecondary: '#000000',
     onBackground: '#000000',
-    // onBackgroundDarker100: 'color-mix(in srgb, var(--on-background) 90%, black)',
-    // onBackgroundDarker200: 'color-mix(in srgb, var(--on-background) 80%, black)',
-    // onBackgroundDarker300: 'color-mix(in srgb, var(--on-background) 70%, black)',
-    // onBackgroundDarker400: 'color-mix(in srgb, var(--on-background) 60%, black)',
-    // onBackgroundDarker500: 'color-mix(in srgb, var(--on-background) 50%, black)',
-    // onBackgroundDarker600: 'color-mix(in srgb, var(--on-background) 40%, black)',
-    // onBackgroundDarker700: 'color-mix(in srgb, var(--on-background) 30%, black)',
-    // onBackgroundDarker800: 'color-mix(in srgb, var(--on-background) 20%, black)',
-    // onBackgroundDarker900: 'color-mix(in srgb, var(--on-background) 10%, black)',
+    onBackgroundDarker100: 'color-mix(in srgb, var(--on-background) 90%, black)',
+    onBackgroundDarker200: 'color-mix(in srgb, var(--on-background) 80%, black)',
+    onBackgroundDarker300: 'color-mix(in srgb, var(--on-background) 70%, black)',
+    onBackgroundDarker400: 'color-mix(in srgb, var(--on-background) 60%, black)',
+    onBackgroundDarker500: 'color-mix(in srgb, var(--on-background) 50%, black)',
+    onBackgroundDarker600: 'color-mix(in srgb, var(--on-background) 40%, black)',
+    onBackgroundDarker700: 'color-mix(in srgb, var(--on-background) 30%, black)',
+    onBackgroundDarker800: 'color-mix(in srgb, var(--on-background) 20%, black)',
+    onBackgroundDarker900: 'color-mix(in srgb, var(--on-background) 10%, black)',
     onBackgroundLighter: 'color-mix(in srgb, var(--on-background) 80%, white)',
     onSurface: '#000000',
 
@@ -107,18 +103,17 @@ export const defaultQuickFormTokens: QuickFormTokens = {
 
     borderColor: 'var(--primary)'
 }
-// export const defineQuickFormTokens = (...tokens: Array<Partial<typeof defaultQuickFormTokens>>) => defineVariables(tokens.reduceRight((n, o) => ({ ...o, ...n }), {}) as typeof defaultQuickFormTokens);
-export const quickformtokens = camelToKebabCase(defaultQuickFormTokens);
+
 
 
 /**
  * Defines and returns the CSS variables for the Quick Form based on provided tokens.
  * This function merges user-defined tokens with the default tokens, ensuring that
  * any customizations are applied on top of the defaults. The result is a flat object
- * where keys are CSS variable names in kebab-case, suitable for direct use in styling.
+ * where keys are CSS variable names in camel-case, suitable for direct use in styling objects that use React.CSSProperties.
  * 
  * @param tokens - An array of token objects. Each object can partially override the default tokens.
- * @returns A flat object with CSS variable names as keys and their corresponding values.
+ * @returns A flat object with CSS camel-case variable names as keys and their corresponding values.
  */
 export const defineQuickFormTokens = (...tokens: Array<Partial<QuickFormTokens>>) => {
     // Merges and overrides default tokens with provided ones in reverse order for precedence.
@@ -127,6 +122,18 @@ export const defineQuickFormTokens = (...tokens: Array<Partial<QuickFormTokens>>
         ...newTokens,
     }), defaultQuickFormTokens);
 
-    // Convert merged tokens to kebab-case CSS variables and return.
-    return camelToKebabCase(mergedTokens);
+    // Ensures merged tokens are camelCase CSS variables that React.CSSProperties can use and return.
+    return defineVariables(mergedTokens);
 };
+
+
+
+
+
+
+/**
+ * Provides QuickForm with css tokens to be passed around in the components so they refer to the same css variables that are loaded into the QuickFormProvider upon application instantiation.
+ * @returns A flat object with CSS variables in camelCase that have corresponding values provided as kebab-case tokens variable names that map to globally defined colors.
+ * See example: quickformtokens = { onPrimary: "var(--on-primary)"; onSecondary: "var(--on-secondary)" } and so on. You get the idea.
+ */
+export const quickformtokens = camelToKebabCase(defaultQuickFormTokens);
