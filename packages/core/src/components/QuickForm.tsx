68b3a452c7488f118fd60545f33f827ad260b578
Kasper Baun <kasperbaun@hotmail.com>
2024-05-07 10:08:47 +0200

    fix: remove useState and useEffect in QuickForm.tsx

--- packages/core/src/components/QuickForm.tsx
+++ packages/core/src/components/QuickForm.tsx
@@ -1,35 +1,26 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from 'react';
 import { useQuickForm } from "../state/QuickFormContext";
 import { Ending, Submit, Intro, SlideRenderer } from "./index";
 
 export const QuickForm: React.FC = () => {
     const { state, setIntroVisited } = useQuickForm();
-    const [component, setComponent] = useState<React.ReactNode>(null);
 
-    useEffect(() => {
-        switch (true) {
-            case state.isIntroSlide && typeof state.data.intro !== "undefined":
-                setComponent(<Intro model={state.data.intro} onBtnClick={setIntroVisited} />);
-                break;
-
-            case state.isSubmitSlide:
-                setComponent(<Submit model={state.data.submit} />);
-                break;
-
-            case state.isEndingSlide:
-                setComponent(<Ending model={state.data.ending} />);
-                break;
-
-            default:
-                setComponent(<SlideRenderer key={state.currIdx} />);
-                break;
+    const renderComponent = () => {
+        if (state.isIntroSlide && typeof state.data.intro !== "undefined") {
+            return <Intro model={state.data.intro} onBtnClick={setIntroVisited} />;
+        } else if (state.isSubmitSlide) {
+            return <Submit model={state.data.submit} />;
+        } else if (state.isEndingSlide) {
+            return <Ending model={state.data.ending} />;
+        } else {
+            return <SlideRenderer key={state.currIdx} />;
         }
-    }, [state, setIntroVisited]); // dependencies array to re-run the effect when state changes
+    };
 
     return (
         <div style={{ width: "100%" }}>
-            {component}
+            {renderComponent()}
         </div>
     );
 };
