85731d403152aab88d64c4fd6c42ee7b783f2c52
Kasper Baun <kasperbaun@hotmail.com>
2024-06-03 14:02:12 +0200

    fix: updated testdata for allInputControlsTest.json

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { QuickFormDefinition } from '../../core/src/model';
-import testdata from "./data/allInputControlsMultipleSlidesTest.json";
+import testdata from "./data/allInputControlsTest.json";
 import { QuickFormProvider } from '../../core/src/state';
 import { NavigationButton, QuickForm } from '../../core/src/components';
 import { QuickFormContainer } from './components';
