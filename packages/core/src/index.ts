d209037026165150da57fab308b4aa5c75a7f63c
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-11-14 20:09:12 +0100

    fix: exported types from model

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,5 +3,5 @@ export * from "./state";
 export * from "./services";
 export * from "./hooks";
 export * from "./style";
-export type { QuickFormDefinition, InputProps, QuestionModel } from "./model";
+export type * from "./model";
 export { defaultQuickFormTokens } from "./style/defaultQuickFormTokens";
