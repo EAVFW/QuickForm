db0b593a442aff24923d8a5be50c0e2bb2033d01
Poul Kjeldager <poul@kjeldager.com>
2026-08-17 18:04:22 +0000

    ci: publish npm packages with trusted publishing

--- release.config.cjs
+++ release.config.cjs
@@ -4,4 +4,16 @@ module.exports = {
         { name: "dev", prerelease: true },
         { name: "vnext", prerelease: true },
     ],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/npm", { npmPublish: false }],
+        [
+            "@semantic-release/exec",
+            {
+                publishCmd: "npm publish --access public --tag <%= nextRelease.channel || 'latest' %>",
+            },
+        ],
+        "@semantic-release/github",
+    ],
 };
